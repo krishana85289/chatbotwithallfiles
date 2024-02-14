1c0d2f2899842942706114d27d39d7131e8355bc
Krishan kumar <40643460+krishana85289@users.noreply.github.com>
2024-02-15 03:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ app.register_blueprint(qa_api_blueprint)
 def index():
     return render_template('index.html')
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0',port=5000)
+    app.run(debug=False)
